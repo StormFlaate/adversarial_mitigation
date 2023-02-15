6335c8ecc3ab9005b49e795f28273abc36791c59
StormFlaate <fflaate@hotmail.no>
2023-02-15 22:58:23 +0100

    updates the docstring and handling inception v3 edge case

--- run_helper.py
+++ run_helper.py
@@ -1,18 +1,33 @@
+import sys
 import torch
-
+from torch.nn import Module
+from torch.utils.data import DataLoader
 from customDataset import ISICDataset # Import the Pytorch library
 
 
 
 def train_model_finetuning(
-        model,
+        model: Module,
         dataset: ISICDataset,
-        data_loader,
-        criterion, 
-        optimizer, 
-        epoch_count:int=20):
-    
+        data_loader: DataLoader,
+        criterion: Module, 
+        optimizer: torch.optim.Optimizer,
+        mode_name: str = "",
+        epoch_count: int = 20) -> Module:
+    """Trains a neural network model by fine-tuning the last layer.
 
+    Args:
+        model: The neural network model to train.
+        dataset: The dataset used for training.
+        data_loader: The data loader used for iterating over the dataset.
+        criterion: The loss function used for calculating the loss between model output and labels.
+        optimizer: The optimizer used for updating the model parameters.
+        mode_name: The type of the model being used (if applicable).
+        epoch_count: The number of epochs to train the model.
+
+    Returns:
+        The trained neural network model.
+    """
     # Freeze the model parameters to prevent backpropagation
     for param in model.parameters():
         param.requires_grad = False
@@ -23,25 +38,54 @@ def train_model_finetuning(
 
     # Move the model to the GPU if GPU is available 
     device = torch.device("cude" if torch.cuda.is_available() else "cpu")
+    print("Is CUDA available:", torch.cuda.is_available())
     model.to(device)
 
 
+    # Loop over the number of epochs
     for epoch in range(epoch_count):
+        # Initialize the running loss for this epoch
         running_loss = 0.0
+        
+        # Loop over the data in the data loader
         for i, data in enumerate(data_loader, 0):
+            # Get the inputs and labels from the data
             inputs, labels = data
+            
+            # Convert labels to a tensor of type float
             labels = torch.tensor(labels, dtype=torch.float)
+            
+            # Clear the gradients in the optimizer
             optimizer.zero_grad()
+            
+            # Move inputs and labels to the specified device
             inputs = inputs.to(device)
             labels = labels.to(device)
-            outputs, x = model(inputs)
+            
+            # If the model type is "inceptionv3", pass inputs through the model and get two outputs
+            if mode_name == "inceptionv3":
+                outputs, x = model(inputs)
+            # Otherwise, pass inputs through the model and get one output
+            else: 
+                outputs = model(inputs)
+            
+            # Calculate the loss between the model output and the labels
             loss = criterion(outputs, labels)
+            
+            # Backpropagate the loss through the model to calculate gradients
             loss.backward()
+            
+            # Update the model parameters using the gradients and the optimizer
             optimizer.step()
+            
+            # Add the loss for this batch to the running loss for this epoch
             running_loss += loss.item()
 
+        # Print the average loss for this epoch
         print('Epoch {} loss: {:.4f}'.format(epoch + 1, running_loss / (i + 1)))
 
+    # Print a message indicating that training has finished
     print('Finished training')
+
 
     return model
