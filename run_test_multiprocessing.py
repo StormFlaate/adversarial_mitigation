1365e5d23b9b99f8bc66bf06da83db424560e43a
StormFlaate <fflaate@hotmail.no>
2023-02-16 22:42:20 +0100

    Add multiprocesing test to find sweet spot for number of workers

---
+++ run_test_multiprocessing.py
@@ -0,0 +1,52 @@
+import sys
+import torch
+import torchvision
+from tqdm import tqdm
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+from time import time
+import multiprocessing as mp
+from config import (
+    IMAGE_FILE_TYPE, IMAGENET_MEAN, IMAGENET_STD, INCEPTIONV3_MODEL_NAME, INCEPTIONV3_PIXEL_SIZE, 
+    LEARNING_RATE, MOMENTUM, TEST_2018_LABELS, TEST_2018_ROOT_DIR, 
+    TRAIN_2018_LABELS, TRAIN_2018_ROOT_DIR, BATCH_SIZE, EPOCH_COUNT, 
+    TRAIN_NROWS, TEST_NROWS
+)
+from customDataset import ISICDataset
+
+
+if __name__ == '__main__':
+    mp.freeze_support()
+    mp.set_start_method('spawn')
+
+    transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.ToTensor()
+    ])
+
+    train_dataset_2018 = ISICDataset(
+        csv_file=TRAIN_2018_LABELS, 
+        root_dir=TRAIN_2018_ROOT_DIR, 
+        transform=transform,
+        image_file_type=IMAGE_FILE_TYPE,
+        nrows=TRAIN_NROWS
+    )
+
+    for num_workers in range(2, mp.cpu_count(), 2):  
+        print("Number of workers:", num_workers)
+        train_loader = DataLoader(
+            train_dataset_2018, 
+            batch_size=BATCH_SIZE,
+            num_workers=num_workers,
+            pin_memory=True
+        )
+        
+        start = time()
+        for epoch in range(1, 3):
+            for i, data in tqdm(enumerate(train_loader, 0)):
+                pass
+        end = time()
+        print("Finish with:{} second, num_workers={}".format(end - start, num_workers))
