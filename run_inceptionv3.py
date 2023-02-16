f916925834963ac67f5e40bc6c3af0ab0de6b4a0
StormFlaate <fflaate@hotmail.no>
2023-02-16 17:42:29 +0100

    Updates the run files for both  resnet18 and inceptionv3 for better readability and to save the model to a file

--- run_inceptionv3.py
+++ run_inceptionv3.py
@@ -1,87 +1,89 @@
-#########################################################
-# ============ IMPORT REQUIRED LIBRARIES ================
-#########################################################
 import sys
-import torch # Import the Pytorch library
-import torchvision # Import the torchvision library
-from torchvision import datasets, transforms # Import the transforms module from torchvision
-
-
+import torch
+import torchvision
+from torchvision import datasets, transforms
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
-from config import LEARNING_RATE, MOMENTUM, TEST_2018_LABELS, TEST_2018_ROOT_DIR, TRAIN_2018_LABELS, TRAIN_2018_ROOT_DIR # Import the Image module from the Python Imaging Library (PIL)
-from config import BATCH_SIZE, EPOCH_COUNT, TRAIN_NROWS, TEST_NROWS
-
+from config import (
+    IMAGE_FILE_TYPE, IMAGENET_MEAN, IMAGENET_STD, INCEPTIONV3_MODEL_NAME, INCEPTIONV3_PIXEL_SIZE, 
+    LEARNING_RATE, MOMENTUM, TEST_2018_LABELS, TEST_2018_ROOT_DIR, 
+    TRAIN_2018_LABELS, TRAIN_2018_ROOT_DIR, BATCH_SIZE, EPOCH_COUNT, 
+    TRAIN_NROWS, TEST_NROWS
+)
 from customDataset import ISICDataset
-from run_helper import test_model, train_model_finetuning # Custom dataset class
+from misc_helper import save_model_to_file, truncated_uuid4
+from run_helper import test_model, train_model_finetuning
 
-# setting the randomness seed
+# Set the randomness seed
 torch.manual_seed(42)
 np.random.seed(42)
 
-
-
-print("Setting up preprocess transforms...")
 # Define image pre-processing steps
 preprocess_inceptionv3 = transforms.Compose([
-    transforms.ToPILImage(), # Removes error
-    transforms.Resize(299), # Resize the image to 299x299 pixels
-    transforms.CenterCrop(299), # Crop the image to 299x299 pixels (removing any extra pixels)
-    transforms.ToTensor(), # Convert the image to a Pytorch tensor
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # Normalize the image using the pre-trained model's mean and standard deviation
+    transforms.ToPILImage(),
+    transforms.Resize(INCEPTIONV3_PIXEL_SIZE),
+    transforms.CenterCrop(INCEPTIONV3_PIXEL_SIZE),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD)
 ])
 
-
-# ================= DATASETS ================= #
-print("Loading in datasets...")
-# Training set 2018 - custom class
+# Load the datasets
+print("Loading datasets...")
 train_dataset_2018_inceptionv3 = ISICDataset(
     csv_file=TRAIN_2018_LABELS, 
     root_dir=TRAIN_2018_ROOT_DIR, 
     transform=preprocess_inceptionv3,
-    image_file_type="jpg",
-    nrows=TRAIN_NROWS # defines the number of rows used, utilized this for testing purposes
-    )
-
-# Test set 2018 - custom class
+    image_file_type=IMAGE_FILE_TYPE,
+    nrows=TRAIN_NROWS
+)
 test_dataset_2018_inceptionv3 = ISICDataset(
     csv_file=TEST_2018_LABELS, 
     root_dir=TEST_2018_ROOT_DIR, 
     transform=preprocess_inceptionv3,
-    image_file_type="jpg",
+    image_file_type=IMAGE_FILE_TYPE,
     nrows=TEST_NROWS
-    )
+)
 
-
-# Define the train data loader
-print("Define the train data loader...")
-data_loader_train_2018 = torch.utils.data.DataLoader(train_dataset_2018_inceptionv3, batch_size=BATCH_SIZE, shuffle=True)
+# Define the data loaders
+print("Defining data loaders...")
+train_2018_data_loader = torch.utils.data.DataLoader(
+    train_dataset_2018_inceptionv3, 
+    batch_size=BATCH_SIZE, 
+    shuffle=True
+)
+test_data_loader = torch.utils.data.DataLoader(
+    test_dataset_2018_inceptionv3, 
+    batch_size=BATCH_SIZE, 
+    shuffle=False
+)
 
 # Load the pretrained Inception v3 model
-print("Load the pretrained Inception v3 model...")
+print("Loading pretrained Inception v3 model...")
 model_inceptionv3 = torch.hub.load('pytorch/vision:v0.10.0', 'inception_v3', pretrained=True)
 
-# Define criterion and optimizer -> do not use adam, since learning rate is so small
+# Define criterion and optimizer
+print("Defining criterion and optimizer...")
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model_inceptionv3.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
 
-
-print("Start training model...")
+# Train the model
+print("Training model...")
 model_inceptionv3 = train_model_finetuning(
     model_inceptionv3, 
     train_dataset_2018_inceptionv3, 
-    data_loader_train_2018,
+    train_2018_data_loader,
     criterion,
     optimizer,
-    model_name="inceptionv3",
+    model_name=INCEPTIONV3_MODEL_NAME,
     epoch_count=EPOCH_COUNT
-    )
+)
 
-# Define the test data loader
-print("Define the test data loader...")
-data_loader_test_2018 = torch.utils.data.DataLoader(test_dataset_2018_inceptionv3, batch_size=BATCH_SIZE, shuffle=False)
+# save the model to file
+train_set_name: str = next(iter(TEST_2018_ROOT_DIR.split("/")), None)
+save_model_to_file(model_inceptionv3, INCEPTIONV3_MODEL_NAME, train_set_name, models_dir="models")
 
 
-print("test the models performance...")
-test_model(model_inceptionv3, test_dataset_2018_inceptionv3, data_loader_test_2018, model_name="inceptionv3")
+# Test the model's performance
+print("Testing the model's performance...")
+test_model(model_inceptionv3, test_dataset_2018_inceptionv3, test_data_loader, model_name=INCEPTIONV3_MODEL_NAME)
