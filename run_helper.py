544eebc0c9b8b7bf14b78ac96628ff4fb324a212
StormFlaate <fflaate@hotmail.no>
2023-02-16 21:57:09 +0100

    test fix

--- run_helper.py
+++ run_helper.py
@@ -53,7 +53,7 @@ def train_model_finetuning(
         for i, data in tqdm(enumerate(data_loader, 0)):
             # Get the inputs and labels from the data
             inputs, labels = data
-            input, labels = input.to(device), labels.to(device)
+            inputs, labels = inputs.to(device), labels.to(device)
             
             # Convert labels to a tensor of type float
             labels = torch.tensor(labels, dtype=torch.float)
