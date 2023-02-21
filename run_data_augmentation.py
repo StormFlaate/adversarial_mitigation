7d6c0ab40227b978712c8dd487caedf8d623e2c4
StormFlaate <fflaate@hotmail.no>
2023-02-21 15:08:07 +0100

    Updates rotation range for data augmentation

--- run_data_augmentation.py
+++ run_data_augmentation.py
@@ -9,10 +9,10 @@ import torchvision
 from torchvision import datasets, transforms
 
 # Import custom modules
-from config import (AUGMENTED_TRAIN_2018_LABELS, AUGMENTED_TRAIN_2018_ROOT_DIR, 
+from config import (AUGMENTED_TRAIN_2018_LABELS, AUGMENTED_TRAIN_2018_ROOT_DIR, MIN_MAX_ROTATION_RANGE, 
                     MIN_NUMBER_OF_EACH_CLASS, RANDOM_HORIZONTAL_FLIP_PROBABILITY, 
-                    RANDOM_VERTICAL_FLIP_PROBABILITY, RANDOM_ROTATION_PROBABILITY, 
-                    TRAIN_2018_LABELS, TRAIN_2018_ROOT_DIR, RANDOM_AFFINE_PROBABILITY)
+                    RANDOM_VERTICAL_FLIP_PROBABILITY, 
+                    TRAIN_2018_LABELS, TRAIN_2018_ROOT_DIR)
 from customDataset import ISICDataset
 from data_exploration_helper import dataset_overview
 from data_augmentation import augment_images_and_save_to_file
@@ -24,7 +24,7 @@ from data_augmentation import augment_images_and_save_to_file
 augmentation_transform = transforms.Compose([
     transforms.RandomHorizontalFlip(p=RANDOM_HORIZONTAL_FLIP_PROBABILITY),
     transforms.RandomVerticalFlip(p=RANDOM_VERTICAL_FLIP_PROBABILITY),
-    transforms.RandomRotation(p=RANDOM_ROTATION_PROBABILITY),
+    transforms.RandomRotation(degrees=MIN_MAX_ROTATION_RANGE),
     transforms.ToPILImage()
 ])
 
