c83b8e8348875d43a1590001b744eef0cfce343c
StormFlaate <fflaate@hotmail.no>
2023-02-21 15:00:20 +0100

    Adds extra data augmentation to the dataset to create a more diversified dataset

--- run_data_augmentation.py
+++ run_data_augmentation.py
@@ -9,11 +9,10 @@ import torchvision
 from torchvision import datasets, transforms
 
 # Import custom modules
-from config import (AUGMENTED_TEST_2018_LABELS, AUGMENTED_TEST_2018_ROOT_DIR, 
-                    AUGMENTED_TRAIN_2018_LABELS, AUGMENTED_TRAIN_2018_ROOT_DIR, 
-                    MIN_NUMBER_OF_EACH_CLASS, RAND_AUGMENT_MAGNITUDE, RAND_AUGMENT_NUM_MAGNITUDE_BINS, RAND_AUGMENT_NUM_OPS, RANDOM_HORIZONTAL_FLIP_PROBABILITY, 
-                    RANDOM_VERTICAL_FLIP_PROBABILITY, TEST_2018_LABELS, TEST_2018_ROOT_DIR, 
-                    TRAIN_2018_LABELS, TRAIN_2018_ROOT_DIR)
+from config import (AUGMENTED_TRAIN_2018_LABELS, AUGMENTED_TRAIN_2018_ROOT_DIR, 
+                    MIN_NUMBER_OF_EACH_CLASS, RANDOM_HORIZONTAL_FLIP_PROBABILITY, 
+                    RANDOM_VERTICAL_FLIP_PROBABILITY, RANDOM_ROTATION_PROBABILITY, 
+                    TRAIN_2018_LABELS, TRAIN_2018_ROOT_DIR, RANDOM_AFFINE_PROBABILITY)
 from customDataset import ISICDataset
 from data_exploration_helper import dataset_overview
 from data_augmentation import augment_images_and_save_to_file
@@ -25,11 +24,10 @@ from data_augmentation import augment_images_and_save_to_file
 augmentation_transform = transforms.Compose([
     transforms.RandomHorizontalFlip(p=RANDOM_HORIZONTAL_FLIP_PROBABILITY),
     transforms.RandomVerticalFlip(p=RANDOM_VERTICAL_FLIP_PROBABILITY),
-    # transforms.RandAugment(num_ops=RAND_AUGMENT_NUM_OPS, magnitude=RAND_AUGMENT_MAGNITUDE, num_magnitude_bins=RAND_AUGMENT_NUM_MAGNITUDE_BINS),
+    transforms.RandomAffine(p=RANDOM_AFFINE_PROBABILITY),
+    transforms.RandomRotation(p=RANDOM_ROTATION_PROBABILITY),
     transforms.ToPILImage()
 ])
-
-
 
 
 augment_images_and_save_to_file(TRAIN_2018_ROOT_DIR, AUGMENTED_TRAIN_2018_ROOT_DIR, TRAIN_2018_LABELS, AUGMENTED_TRAIN_2018_LABELS, augmentation_transform, min_number_of_each_class=MIN_NUMBER_OF_EACH_CLASS)
