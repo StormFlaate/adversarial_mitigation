6d5502ef7a9b0a03e403c4af18a26c186416f7a8
StormFlaate <fflaate@hotmail.no>
2023-02-23 12:59:59 +0100

    Creates different implementation for the 2018 and 2019 dataset

--- run_model.py
+++ run_model.py
@@ -2,14 +2,14 @@
 import sys
 import torch
 import torchvision
-from torch.utils.data import random_split, Dataset, Subset
+from torch.utils.data import Dataset, Subset, Dataset
 from torchvision import datasets, transforms
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
 import multiprocessing as mp
 from config import (
-    GAMMA, MODEL_NAME, NUM_WORKERS, PIN_MEMORY_TRAIN_DATALOADER, PREPROCESS_TRANSFORM, 
+    GAMMA, IS_2018_DATASET, MODEL_NAME, NUM_WORKERS, PIN_MEMORY_TRAIN_DATALOADER, PREPROCESS_TRANSFORM, 
     SHUFFLE_TRAIN_DATALOADER, 
     STEP_SIZE, TEST_DATASET_LABELS, TEST_DATASET_ROOT_DIR, SHUFFLE_VAL_DATALOADER, TEST_SPLIT_PERCENTAGE,
     TRAIN_DATASET_LABELS, TRAIN_DATASET_ROOT_DIR, IMAGE_FILE_TYPE,
@@ -17,7 +17,7 @@ from config import (
     EPOCH_COUNT, TRAIN_SPLIT_PERCENTAGE, VAL_BATCH_SIZE, VAL_SPLIT_PERCENTAGE)
 from customDataset import ISICDataset
 from misc_helper import save_model_and_parameters_to_file
-from run_helper import get_category_counts, test_model, train_model
+from run_helper import get_category_counts, test_model, train_model, random_split
 
 if __name__ == '__main__':
     mp.freeze_support()
@@ -38,21 +38,28 @@ if __name__ == '__main__':
         nrows=TRAIN_NROWS
     )
 
-    # Splits the dataset into train and validation
-    train_dataset, val_dataset = random_split(train_dataset_full, [TRAIN_SPLIT_PERCENTAGE, VAL_SPLIT_PERCENTAGE])    
+
+    # the 2018 and 2019 dataset has some configuration differences when it comes to train, validation and test datasets
+    if IS_2018_DATASET:
+        # Splits the dataset into train and validation
+        train_dataset, val_dataset = random_split(train_dataset_full, [TRAIN_SPLIT_PERCENTAGE, VAL_SPLIT_PERCENTAGE])    
+
+        test_dataset_full = ISICDataset(
+            csv_file=TEST_DATASET_LABELS, 
+            root_dir=TEST_DATASET_ROOT_DIR, 
+            transform=PREPROCESS_TRANSFORM,
+            image_file_type=IMAGE_FILE_TYPE,
+            nrows=TEST_NROWS
+        )
+        # added for consistency
+        test_dataset = Subset(test_dataset_full, indices=[x for x in range(len(test_dataset_full))])
+    else:
+        train_dataset, val_dataset, test_dataset = random_split(train_dataset_full, [TRAIN_SPLIT_PERCENTAGE, VAL_SPLIT_PERCENTAGE, TEST_SPLIT_PERCENTAGE])
+
+
     print(f"Train dataset length: {len(train_dataset)}")
     print(f"Validation dataset length: {len(val_dataset)}")
-
-    test_dataset_full = ISICDataset(
-        csv_file=TEST_DATASET_LABELS, 
-        root_dir=TEST_DATASET_ROOT_DIR, 
-        transform=PREPROCESS_TRANSFORM,
-        image_file_type=IMAGE_FILE_TYPE,
-        nrows=TEST_NROWS
-    )
-    
-    # added for consistency
-    test_dataset = Subset(test_dataset_full, indices=[x for x in range(len(test_dataset_full))])
+    print(f"Test dataset length: {len(test_dataset)}")
 
 
     print("Defining train data loaders...")
@@ -73,6 +80,7 @@ if __name__ == '__main__':
         num_workers=NUM_WORKERS,
         pin_memory=PIN_MEMORY_TRAIN_DATALOADER
     )
+
 
     print("Define the test data loader...")
     data_loader_test = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False, pin_memory=True)
@@ -101,6 +109,7 @@ if __name__ == '__main__':
     # optimizer = torch.optim.Adam(cnn_model.parameters(), lr=LEARNING_RATE)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=STEP_SIZE, gamma=GAMMA)
 
+    sys.exit()
 
     # Train the model
     print("Training model...")
