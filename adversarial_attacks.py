ca30e2b72a88ee42c0795f2264114d19eacab771
StormFlaate <fflaate@hotmail.no>
2023-03-08 13:35:06 +0100

    Adds a adversarial_attack_helper file for helper functions

--- adversarial_attacks.py
+++ adversarial_attacks.py
@@ -10,6 +10,7 @@ train_data_loader, val_data_loader, test_data_loader = get_data_loaders(batch_si
 print("get trained or deafult model...")
 model = get_trained_or_default_model(model_file_name="resnet18_data_ISIC2018_Training_Input_2023-03-03__ff2.pt")
 
+
 attack = torchattacks.FGSM(model, eps=2/255)
 
 for index, (input, true_label) in  tqdm(enumerate(train_data_loader)):
@@ -23,13 +24,19 @@ for index, (input, true_label) in  tqdm(enumerate(train_data_loader)):
     true_label_argmax = torch.argmax(true_label, 1)
 
     # runs the optimzation on the input given the true label
-    adversarial_input = attack(input, true_label)
-
+    adversarial_input = attack(input, true_label_argmax)
     predicted_label = model(adversarial_input)
 
-    np_label = true_label_argmax.detach().cpu().numpy()
-    np_predicted = predicted_label.detach().cpu().numpy()
 
-    print(f"True: {np.argmax(np_label)}")
-    print(f"Pred: {np.argmax(np_predicted)}")
+    np_true_label = true_label_argmax.detach().cpu().numpy()
+    np_predicted_label = predicted_label.detach().cpu().numpy()
+    
+
+    print("true_label_argmax", true_label_argmax)
+    print("predicted_label", predicted_label)
+    print("np_true_label", np_true_label)
+    print("np_predicted_label", np_predicted_label)
+
+    print(f"True: {np.argmax(np_true_label)}")
+    print(f"Pred: {np.argmax(np_predicted_label)}")
     break
