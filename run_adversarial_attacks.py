847bd1ef54890d4bf48fcbd425218e4c568e95b7
StormFlaate <fflaate@hotmail.no>
2023-03-20 18:36:22 +0100

    Adds adversarial accuracy along with normal accuracy

--- run_adversarial_attacks.py
+++ run_adversarial_attacks.py
@@ -20,29 +20,39 @@ attack = torchattacks.FGSM(model, eps=2/255)
 
 correct_labels: list[int] = []
 predicted_labels: list[int] = []
+predicted_adversarial_labels: list[int] = []
+
 
 for index, (input, true_label) in  tqdm(enumerate(train_data_loader)):
 
     adversarial_input = generate_adversarial_input(input, true_label, attack)
-
-    predicted_label = model(adversarial_input)
+    
+    predicted_label = model(input)
+    predicted_adversarial_label = model(adversarial_input)
 
 
     np_true_label = true_label.detach().cpu().numpy()
     np_predicted_label = predicted_label.detach().cpu().numpy()
+    np_predicted_adversarial_label = predicted_adversarial_label.detach().cpu().numpy()
     
 
     # print("true_label_argmax", true_label)
-    # print("predicted_label", predicted_label)
+    # print("predicted_adversarial_label", predicted_adversarial_label)
     # print("np_true_label", np_true_label)
     # print("np_predicted_label", np_predicted_label)
     correct_argmax_label = np.argmax(np_true_label)
     predicted_argmax_label = np.argmax(np_predicted_label)
+    predicted_adversarial_argmax_label = np.argmax(np_predicted_adversarial_label)
 
     correct_labels.append(correct_argmax_label)
     predicted_labels.append(predicted_argmax_label)
+    predicted_adversarial_labels.append(predicted_adversarial_argmax_label)
 
 
 
 overall_accuracy = accuracy_score(correct_labels, predicted_labels)
-print(overall_accuracy)
+
+overall_adversarial_accuracy = accuracy_score(correct_labels, predicted_adversarial_labels)
+
+print("Overall accuracy: ", overall_accuracy)
+print("Overall adversarial accuracy: ", overall_adversarial_accuracy)
