daf0604d71cb018ef6b61cb0a3f403fcd48cb77b
StormFlaate <fflaate@hotmail.no>
2023-04-17 13:14:04 +0200

    Refactor adversarial code for readability

--- run_adversarial_attacks.py
+++ run_adversarial_attacks.py
@@ -1,117 +1,95 @@
-from matplotlib import pyplot as plt
 import numpy as np
-from sklearn.metrics import accuracy_score
 import torch
 import torchattacks
+from sklearn.metrics import accuracy_score
 from tqdm import tqdm
+
 from helper_functions.adversarial_attacks_helper import (
     extract_feature_map_of_convolutional_layers,
-    extract_kernels_from_resnet_architecture,
     generate_adversarial_input,
     visualize_feature_map_of_convolutional_layers
 )
 from helper_functions.misc_helper import get_trained_or_default_model
 from helper_functions.train_model_helper import get_data_loaders
 
-print("get data loaders...")
-train_data_loader, val_data_loader, test_data_loader = get_data_loaders(
-    batch_size=1, 
-    num_workers=1)
+def _initialize_data_loaders():
+    print("get data loaders...")
+    return get_data_loaders(batch_size=1, num_workers=1)
 
-print("get trained or deafult model...")
-model = get_trained_or_default_model(
-    model_file_name="resnet18_augmented_data_ISIC2018_Training_Input_2023-03-08_50__bb6.pt")
-
-
-attack = torchattacks.FGSM(model, eps=2/255)
-
-correct_labels: list[int] = []
-predicted_labels: list[int] = []
-predicted_adversarial_labels: list[int] = []
-model_weights: list = [] # we will save the conv layer weights in this list
-conv_layers: list = [] # we will save the 49 conv layers in this list
-model_children: list = list(model.children()) # get all the model children as list
-
-
-for index, (input, true_label) in  tqdm(enumerate(train_data_loader)):
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    input = input.to(device)
-    true_label = true_label.to(device)  
-
-    model_weights, conv_layers = extract_kernels_from_resnet_architecture(
-        list(model.children()), [], []
+def _initialize_model():
+    print("get trained or default model...")
+    return get_trained_or_default_model(
+        model_file_name="resnet18_augmented_data_ISIC2018_Training_Input_2023-03-08_50__bb6.pt"
     )
-    
-    before_attack: list = extract_feature_map_of_convolutional_layers(
-        input, conv_layers)
+
+def _initialize_attack(model):
+    return torchattacks.FGSM(model, eps=2/255)
+
+def _initialize_device():
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def _process_batch(model, device, input, true_label, attack):
+    input = input.to(device)
+    true_label = true_label.to(device)
+
+    before_attack = extract_feature_map_of_convolutional_layers(input, model)
     visualize_feature_map_of_convolutional_layers(before_attack, "before")
 
     adversarial_input = generate_adversarial_input(input, true_label, attack)
-
-    # predicting with adversarial and benign input    
     predicted_label = model(input)
     predicted_adversarial_label = model(adversarial_input)
 
-
-    model_weights, conv_layers = extract_kernels_from_resnet_architecture(
-        list(model.children()), [], []
-    )
-
-    after_attack: list = extract_feature_map_of_convolutional_layers(
-        adversarial_input, conv_layers)
+    after_attack = extract_feature_map_of_convolutional_layers(adversarial_input, model)
     visualize_feature_map_of_convolutional_layers(after_attack, "after")
 
-    logarithmic_distances = []
-
-    for weights_before_attack, weights_after_attack in zip(before_attack, after_attack):
-        # Flatten tensors
-        flat_weights_before_attack = weights_before_attack.view(-1)
-        flat_weights_after_attack = weights_after_attack.view(-1)
-
-        # Calculate element-wise difference
-        difference = flat_weights_after_attack - flat_weights_before_attack
-        print(difference)
-
-        # Calculate logarithmic distance
-        logarithmic_distance = torch.mean(torch.log(torch.abs(difference) + 1e-8))
-        logarithmic_distances.append(logarithmic_distance.item())
-
+    logarithmic_distances = _calculate_logarithmic_distances(before_attack, after_attack)
     print(
-        "Logarithmic distances between feature map before and after adversarial attack:",
-        logarithmic_distances
+        "Logarithmic distances between feature map before and after adversarial attack:"
+        , logarithmic_distances
     )
 
-    # take a look at the conv layers and the respective weights
-    # for weight, conv in zip(model_weights, conv_layers):
-    #     # print(f"WEIGHT: {weight} \nSHAPE: {weight.shape}")
-    #     print(f"CONV: {conv} ====> SHAPE: {weight.shape}")
+    return (
+        np.argmax(true_label.detach().cpu().numpy()),
+        np.argmax(predicted_label.detach().cpu().numpy()),
+        np.argmax(predicted_adversarial_label.detach().cpu().numpy())
+    )
 
+def _calculate_logarithmic_distances(before_attack, after_attack):
+    distances = []
 
-    np_true_label = true_label.detach().cpu().numpy()
-    np_predicted_label = predicted_label.detach().cpu().numpy()
-    np_predicted_adversarial_label = predicted_adversarial_label.detach().cpu().numpy()
-    
-    correct_argmax_label = np.argmax(np_true_label)
-    predicted_argmax_label = np.argmax(np_predicted_label)
-    predicted_adversarial_argmax_label = np.argmax(np_predicted_adversarial_label)
+    for weights_before_attack, weights_after_attack in zip(before_attack, after_attack):
+        flat_weights_before_attack = weights_before_attack.view(-1)
+        flat_weights_after_attack = weights_after_attack.view(-1)
+        difference = flat_weights_after_attack - flat_weights_before_attack
+        logarithmic_distance = torch.mean(torch.log(torch.abs(difference) + 1e-8))
+        distances.append(logarithmic_distance.item())
 
-    correct_labels.append(correct_argmax_label)
-    predicted_labels.append(predicted_argmax_label)
-    predicted_adversarial_labels.append(predicted_adversarial_argmax_label)
+    return distances
 
-    
-    break
+def main():
+    train_data_loader, _, _ = _initialize_data_loaders()
+    model = _initialize_model()
+    attack = _initialize_attack(model)
+    device = _initialize_device()
 
+    correct_labels = []
+    predicted_labels = []
+    predicted_adversarial_labels = []
 
+    for _, (input, true_label) in tqdm(enumerate(train_data_loader)):
+        correct_label, predicted_label, predicted_adversarial_label = _process_batch(
+            model, device, input, true_label, attack
+        )
+        correct_labels.append(correct_label)
+        predicted_labels.append(predicted_label)
+        predicted_adversarial_labels.append(predicted_adversarial_label)
 
+        break
 
-overall_accuracy = accuracy_score(correct_labels, predicted_labels)
+    overall_accuracy = accuracy_score(correct_labels, predicted_labels)
+    overall_adversarial_accuracy = accuracy_score(
+        correct_labels, predicted_adversarial_labels
+    )
 
-overall_adversarial_accuracy = accuracy_score(
-    correct_labels,
-    predicted_adversarial_labels
-)
-
-print("Overall accuracy: ", overall_accuracy)
-print("Overall adversarial accuracy: ", overall_adversarial_accuracy)
+    print("Overall accuracy: ", overall_accuracy)
+    print("Overall adversarial accuracy: ", overall_adversarial_accuracy)
