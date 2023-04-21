cc115ccaf13f96e1951fa552c78a2539b80bdc93
StormFlaate <fflaate@hotmail.no>
2023-04-21 12:53:03 +0200

    Calculates shape of pred and true labels

--- helper_functions/train_model_helper.py
+++ helper_functions/train_model_helper.py
@@ -503,8 +503,9 @@ def validate_model_accuracy_f1(model, val_data_loader, device):
 
             outputs = model(inputs)
             _, preds = torch.max(outputs, 1)  # Get the class with the highest probability as predictions
+            true_label = torch.max(labels, 1)  # Get the class with the highest probability as predictions
 
-            true_labels.extend(labels.cpu().numpy())
+            true_labels.extend(true_label.cpu().numpy())
             predicted_labels.extend(preds.cpu().numpy())
 
     print(type(predicted_labels))
