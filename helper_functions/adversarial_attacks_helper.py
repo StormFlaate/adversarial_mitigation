5d9f818029e893adfafc1da0fb4bb774c7237d7e
StormFlaate <fflaate@hotmail.no>
2023-05-05 14:37:15 +0200

    removes argmax from adv label

--- helper_functions/adversarial_attacks_helper.py
+++ helper_functions/adversarial_attacks_helper.py
@@ -285,13 +285,13 @@ def generate_adversarial_input(
     label = label.to(device)    
 
     # turns 1-dimensional list into 0-dimensional scalar, needed for attack
-    label_argmax = torch.argmax(label, 1)
+    # label_argmax = torch.argmax(label, 1)
     
-    if attack_name == "deepfool":
-        label_argmax = (label_argmax+1)%label_argmax
+    # if attack_name == "deepfool":
+    #     label_argmax = (label_argmax+1)%label_argmax
 
     # generate the adver
-    adversarial_input = attack(input, label_argmax)
+    adversarial_input = attack(input, label)
     
     return adversarial_input
 
